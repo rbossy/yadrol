ba791d3152c10b0d369cc2ec0360ab23e49c984c
Robert Bossy <grauntlet@gmail.com>
2017-03-08 09:04:46 +0100

    moved local site server to Makefile

--- docs/serve.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-bundle exec jekyll serve
-
-
-# Script for testing pages locally offline.
-# See: https://help.github.com/articles/setting-up-your-github-pages-site-locally-with-jekyll/
